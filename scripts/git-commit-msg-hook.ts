723b888404f499066b9afc954315ae24b8574893
Benjamin Macher <benjamin.dev@icloud.com>
2020-11-16 19:17:15 +0100

    feat: add git-commit-msg-hook

--- scripts/git-commit-msg-hook.ts
+++ scripts/git-commit-msg-hook.ts
@@ -1,1 +1,33 @@
-console.info(`Hello from ${__filename.split('/').pop()} script!`);
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+const { info, error } = console;
+
+const rootPath = resolve(__dirname, '..');
+
+const maxMsgLength = 50;
+const commitRE = new RegExp(`^(revert: )?(feat|fix|docs|refactor|test|chore|wip|style|tooling)(\\(.+\\))?: .{1,${maxMsgLength}}`);
+
+// First two arguments are path to ts-node and path to script
+const [,,msgPath] = process.argv;
+
+const msg = readFileSync(resolve(rootPath, msgPath))
+  .toString()
+  .trim();
+
+info('Checking commit message');
+
+if (!commitRE.test(msg)) {
+  info();
+
+  let errorMsg = 'Error: invalid commit message format.\n\n';
+  errorMsg += '    A propper commit message would look like this:\n\n';
+  errorMsg += '    feat(package-a): add a feature\n';
+  errorMsg += '    fix(package-b): error (close: #123)\n';
+
+  error(errorMsg);
+
+  process.exit(1);
+}
+
+info('✅ Correct message format');
