15bd82b9e96c5cc1b465b513ded04aae984caf7d
Benjamin Macher <benjamin.dev@icloud.com>
2020-11-16 17:28:14 +0100

    tooling(npm-post-install-hook): now resolves path with __dirname

--- scripts/npm-post-install-hook.ts
+++ scripts/npm-post-install-hook.ts
@@ -11,16 +11,13 @@ const { info, error } = console;
 const rootPath = resolve(__dirname, '..');
 const gitHooksPath = resolve(rootPath, '.git/hooks');
 
-shell.rm('-f', resolve(gitHooksPath, 'commit-msg'));
-shell.rm('-f', resolve(gitHooksPath, 'pre-commit'));
-shell.rm('-f', resolve(gitHooksPath, 'pre-push'));
-
 function installHookOrThrow(path: string, name: string) {
   info(`Installing: ${name}`);
 
   const hookPath = `${path}/${name}`;
+  const hookScriptPath = resolve(__dirname, `git-${name}-hook.ts`);
 
-  let { code } = shell.exec(`echo "npx ts-node $(git rev-parse --show-toplevel)/scripts/git-${name}-hook.ts" > ${hookPath}`);
+  let { code } = shell.exec(`echo "npx ts-node ${hookScriptPath}" > ${hookPath}`);
 
   if (code !== 0) {
     throw new Error(`Error: Couldn't add ${name} hook`);
@@ -43,7 +40,7 @@ const hooks = shell
   // Extract hook name -> git-<name>-hook.ts
   .map((hook) => hook.slice(4, -8));
 
-info('Checking, whether git hooks are already installed.');
+info('Checking, whether git hooks are already installed');
 const installedHooks = shell.ls(gitHooksPath);
 const toBeInstalledHooks: string[] = [];
 
@@ -73,4 +70,4 @@ if (toBeInstalledHooks.length > 0) {
   }
 }
 
-info('\nDone!');
+info('\n✅ Done!');
